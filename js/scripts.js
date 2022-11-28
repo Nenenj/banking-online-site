76185df87f6b3b4551820f2cb8a691af3dcc0c8f
Nenenj <nnennanjoku08@gmail.com>
2022-11-28 14:30:10 +0100

    Complete update

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    // Activate Carousel
+    $('#mycarousel').carousel({interval: 2000 });
+    //Enable Carousel
+    $('#carouselButton').click(function() {
+        if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }  
+});
+
+  
+
+$("#myModal").click(function() {
+    $("#myModal").modal('toggle');
+});  
+
+});
+   
+
